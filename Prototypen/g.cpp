617d6c5270e82d2d52a30593087eaf202af929c9
Martin Ueding <dev@martin-ueding.de>
2013-09-06 13:32:13 +0200

    Multiplikator neu geschrieben

--- Prototypen/g.cpp
+++ Prototypen/g.cpp
@@ -4,20 +4,40 @@
 #include <cstdio>
 
 int main() {
-	char a = 0x80;
-	char b = 0x02;
-	short temp = 0x00;
-	char x, r;
+	char a = 0x08; // Eingabe Faktor 1.
+	char b = 0x80; // Eingabe Faktor 2.
+	char x, r; // Ausgaberegister.
+	char c;
+
+	// Deklariere 2 Byte Speicher für die Register HL sowie BC.
+	short hl;
+	char *l = (char *) &hl;
+	char *h = ((char *) &hl + 1);
+	short de;
+	char *e = (char *) &de;
+	char *d = ((char *) &de + 1);
+
+	c = a; // Sicherungskopie des Zählers.
+
+	*l = 0x00; // Setzte L und H auf Anfangswerte.
+	*h = 0x00;
+
+	*d = 0x00;
+	*e = b;
 
 top:
-	temp += b;
+	hl = hl + de; // Addierschritt.
+	printf("HL = %04x, DE = %04x\n", hl, de);
+
+	a = c;
 	--a;
-	if (a > 0)
+	c = a;
+	if (a)
 		goto top;
 
-	x = temp;
-	r = temp >> 8;
+	x = *l;
+	r = *h;
 
-	printf("%02x %02x\n", r & 0xFF, x & 0xFF);
+	printf("X = %02x, R = %02x\n", x & 0xFF, r & 0xFF);
 	return 0;
 }
